4eb8db9f208adc3654d78ec925d63e8a21e85d59
AlcidesThiago2016 <alcides_thiago@hotmail.com>
2024-11-11 14:20:16 -0300

    Create Function remove tarefa

--- src/store/reducers/tarefas.ts
+++ src/store/reducers/tarefas.ts
@@ -4,32 +4,34 @@ import * as enums from '../../utils/enums/Tarefa'
 
 const tarefasSlice = createSlice({
   name: 'tarefas',
-  initialState: [
-    new Tarefa(
-      'Estudar Java Script',
-      enums.Prioridade.IMPORTANTE,
-      enums.Status.PENDENTE,
-      '',
-      1
-    ),
-    new Tarefa(
-      'Estudar TypeScript',
-      enums.Prioridade.URGENTE,
-      enums.Status.CONCLUIDA,
-      'Rever aula 2 do módulo',
-      2
-    ),
-    new Tarefa(
-      'Estudar React',
-      enums.Prioridade.URGENTE,
-      enums.Status.PENDENTE,
-      'Praticar o useEffect',
-      3
-    )
-  ],
+  initialState: {
+    itens: [
+      new Tarefa(
+        'Estudar Java Script',
+        enums.Prioridade.IMPORTANTE,
+        enums.Status.PENDENTE,
+        '',
+        1
+      ),
+      new Tarefa(
+        'Estudar TypeScript',
+        enums.Prioridade.URGENTE,
+        enums.Status.CONCLUIDA,
+        'Rever aula 2 do módulo',
+        2
+      ),
+      new Tarefa(
+        'Estudar React',
+        enums.Prioridade.URGENTE,
+        enums.Status.PENDENTE,
+        'Praticar o useEffect',
+        3
+      )
+    ]
+  },
   reducers: {
     remover: (state, action: PayloadAction<number>) => {
-      state = state.filter((tarefa) => tarefa.id !== action.payload)
+      state.itens = state.itens.filter((tarefa) => tarefa.id !== action.payload)
     }
   }
 })
